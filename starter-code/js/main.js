14a41946a21264d904087febc25792cbf24c0eb6
Sera Bak <serabak15@gmail.com>
2016-08-10 16:07:30 -0700

    added conditionals and alerts to main.js

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -4,3 +4,25 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} 
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} 
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	alert("You found a match!");
+} 
+else {
+	alert("Sorry, try again.");
+}
+
