433418e7499078d6abd86da3166dd19e5cac39f4
Sera Bak <serabak15@gmail.com>
2016-08-14 23:52:23 -0700

    dynamically added cards to the board with main.js

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -5,24 +5,38 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.");
-}
+// if (cardTwo === cardFour) {
+// 	alert("You found a match!");
+// } 
+// else {
+// 	alert("Sorry, try again.");
+// }
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.");
-}
+// if (cardOne === cardTwo) {
+// 	alert("You found a match!");
+// } 
+// else {
+// 	alert("Sorry, try again.");
+// }
 
-if (cardThree === cardFour) {
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.");
-}
+// if (cardThree === cardFour) {
+// 	alert("You found a match!");
+// } 
+// else {
+// 	alert("Sorry, try again.");
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < 4; i++) {
+		
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card'; 
+		gameBoard.appendChild(cardElement);
+	};
+};
+
+createBoard();
+
 
